bec05f8d1db5fede9de275fd2cf7d6e226fc3baa
Slyamshaihov Yerdos <57892816+erdosslm@users.noreply.github.com>
2019-11-21 12:44:04 +0600

    Add files via upload

---
+++ 58 task.cpp
@@ -0,0 +1,30 @@
+﻿//58
+# include <iostream>
+using namespace std;
+int main ()
+{
+  int d;
+  int m;
+  int y;
+  int d2;
+  int m2;
+  int y2;
+  cin>>d>>m>>y;
+  cin>>d2>>m2>>y2;
+  if (y>y2){
+      cout<<"No";
+  } else if (y2>y){
+      cout<<"Yes";
+  } else {
+      if (m>m2){
+      cout<<"No";
+  } else if (m2>m){
+      cout<<"Yes";
+  } else {
+      if (d>d2){
+      cout<<"No";
+  } else if (d2>d){
+      cout<<"Yes";
+  } else {
+      cout<<"No";
+  }
